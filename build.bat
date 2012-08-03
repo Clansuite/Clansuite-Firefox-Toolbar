928058d93e1fa4daea96b6411691d9f9026b9816
jakoch <jakoch@web.de>
2012-08-03 19:53:43 +0200

    fixed build script

--- build.bat
+++ build.bat
@@ -1,24 +1,25 @@
 ::   ,------------------------------------.
 ::   | Clansuite Toolbar Build-Script w32 |
-::   | by Jens-André Koch (jakoch@web.de) |
-::   | for clansuite.com                  |
+::   | by Jens-André Koch <jakoch@web.de> |
+::   | http://clansuite.com/              |
 ::   | LICENCE: GNU/GPLv2 or any later <3 |
 ::   `------------------------------------'
 
 :: 7zip is needed as packaging tool
 :: Download CLI Version: http://downloads.sourceforge.net/sevenzip/7za920.zip
-set "7zip=C:\Programme\7-Zip\7z.exe"
+set "zipEXE=C:\Programme\7-Zip\7z.exe"
 
 :: delete any prior version
 del clansuite_toolbar*.xpi
 
-:: 7zip command line options for packaging clansuite_toolbar.xpi
+:: description of the 7zip command line option
+:: for packaging "clansuite_toolbar.xpi":
 ::
 ::  a               = archiv
 ::  tzip            = zip packaging
 ::  r               = recurse into subdirectories
 ::  mx              = compression level
-::  archiv name 
+::  archiv name
 ::  -x!.svn         = exclude SVN dir
 ::  -x!*.bat        = exclude *.bat (the make file)
 ::  -x!*.sh         = exclude *.sh (the make file)
@@ -29,6 +30,6 @@ del clansuite_toolbar*.xpi
 ::  *               = all files & folders
 ::
 set "excludes=-x!.svn -x!*.bat -x!*.sh -x!*.xhtml -x!*.xpi -x!update.xml -x!update.rdf"
-%7zip% a -tzip -r -mx=9 clansuite_toolbar-v.xpi %excludes% *
+%zipEXE% a -tzip -r -mx=9 clansuite_toolbar-v.xpi %excludes% *
 
 pause
